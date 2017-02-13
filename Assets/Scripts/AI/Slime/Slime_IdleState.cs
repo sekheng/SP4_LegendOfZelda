4e8db18ddcd9410f452c80abbc80c1a23f07000f
unknown <150592K@M313G1102.sidm.student.local>
2017-02-13 17:52:04 +0800

    Slime stuff, need to fill in what the state does tommorrow

---
+++ Assets/Scripts/AI/Slime/Slime_IdleState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slime_IdleState : State {
+    private float timeAwayToRoam;
+    void Start()
+    {
+        timeAwayToRoam = 0;
+    }
+    public override void UpdateState()
+    {
+        timeAwayToRoam += Time.deltaTime;
+        //TODO change state to roam and maybe have random time 
+        if(timeAwayToRoam > 5.0f)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Slime/Slime_Statemanager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slime_Statemanager : MonoBehaviour {
+
+    public Slime_IdleState idle;
+    public Slime_RoamState roam;
+    public Slime_DeadState dead;
+
+    private State currState;
+
+    void Start(){
+        currState = idle;
+    }
+    
+    void Update()
+    {
+        currState.UpdateState();
+    }
+}
