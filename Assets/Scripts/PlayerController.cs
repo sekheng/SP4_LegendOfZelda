4f119ccb6df6b0e0ff33aefee10d18945a3fd035
Ah Heng <leesekheng@yahoo.com>
2017-02-13 13:59:10 +0800

    Solve player's movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Used for keyboard and android mobile controller!
+/// Too lazy to create another script for android mobile
+/// Will add for mobile soon!
+/// </summary>
 public class PlayerController : MonoBehaviour {
 
 
-    private int sumOfHorizontal = 0;
-    private int sumOfVertical = 0;
+    //private int sumOfHorizontal = 0;
+    //private int sumOfVertical = 0;
 
-    private int up, down, left, right;
+    //private int up, down, left, right;
+
+    // There will only be 1 hero throughout the entire game!
+    private HeroesMovement theOnlyHero;
+    // This is to check if the player is not moving the character, then stop the movement!
+    private bool checkPlayerMoved = false;
 
 	// Use this for initialization
 	void Start () {
-	    up = down = left = right = 0;
+        //up = down = left = right = 0;
+        theOnlyHero = GameObject.FindGameObjectWithTag("Player").GetComponent<HeroesMovement>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        up = Input.GetKey("w") ? 1 : 0;
-        down = Input.GetKey("s") ? -1 : 0;
-        left = Input.GetKey("a") ? -1 : 0;
-        right = Input.GetKey("d") ? 1 : 0;
+        //up = Input.GetKey("w") ? 1 : 0;
+        //down = Input.GetKey("s") ? -1 : 0;
+        //left = Input.GetKey("a") ? -1 : 0;
+        //right = Input.GetKey("d") ? 1 : 0;
 
-        sumOfHorizontal = left + right;
-        sumOfVertical = up + down;
+        //sumOfHorizontal = left + right;
+        //sumOfVertical = up + down;
 
-        transform.position += new Vector3(sumOfHorizontal * 5, sumOfVertical * 5, 0) * Time.deltaTime;
-	}
+        //transform.position += new Vector3(sumOfHorizontal * 5, sumOfVertical * 5, 0) * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            theOnlyHero.moveDirection(new Vector2(0, 1));
+            checkPlayerMoved = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            theOnlyHero.moveDirection(new Vector2(0, -1));
+            checkPlayerMoved = true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            theOnlyHero.moveDirection(new Vector2(-1, 0));
+            checkPlayerMoved = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            theOnlyHero.moveDirection(new Vector2(1, 0));
+            checkPlayerMoved = true;
+        }
+    }
+
+    void LateUpdate()
+    {
+        switch (checkPlayerMoved)
+        {
+            case false:
+                // If player hasn't move, stop the hero's movement!
+                theOnlyHero.stopMovement();
+                break;
+            case true:
+                // Then initialized it to be false!
+                checkPlayerMoved = false;
+                //Debug.Log("Reinitializing");
+                break;
+        }
+    }
 }
--- Assets/Scripts/Sek Heng/HeroesMovement.cs
+++ Assets/Scripts/Sek Heng/HeroesMovement.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// For Different Heroes Movement!
+/// So don't go and mess with RigidBody2D!!
+/// </summary>
 public class HeroesMovement : MonoBehaviour {
+    [Tooltip("Speed of the hero")]
+    public float m_speed = 5;
+
+    // Every hero has a rigid body for collision!
+    private Rigidbody2D heroPhysics;
+
+
 
 	// Use this for initialization
 	void Start () {
-	
+        heroPhysics = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    // We will use this to move the player
+    public void moveDirection(Vector2 zeDir)
+    {
+        // Applying the direction then the speed!
+        heroPhysics.velocity = zeDir.normalized * m_speed;
+    }
+
+    // This will instantly set the velocity of hero to be 0!
+    public void stopMovement()
+    {
+        // constantly allocating memory is bad!
+        if (!Mathf.Approximately(0, heroPhysics.velocity.sqrMagnitude))
+        {
+            heroPhysics.velocity = new Vector2(0, 0);
+            //Debug.Log("Hero has stop moving!");
+        }
+    }
 }
