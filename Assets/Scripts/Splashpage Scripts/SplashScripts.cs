ce3612c82f1ba000e6a253e57a147eb87e649c2a
Ah Heng <leesekheng@yahoo.com>
2017-02-14 16:54:26 +0800

    Doing a simple splashpage

---
+++ Assets/Scripts/Splashpage Scripts/SplashScripts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The logic of the splash scene!
+/// TODO: Make it complicated!
+/// </summary>
+public class SplashScripts : MonoBehaviour {
+    [Tooltip("Set the time for this splash")]
+    public float m_timer = 3.0f;
+    private float timeCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCounter += Time.deltaTime;
+        if (timeCounter > m_timer)
+            gameObject.GetComponent<GoSceneScript>().GoNextScene(); // Will change soon!
+	}
+}
