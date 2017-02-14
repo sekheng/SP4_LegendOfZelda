8297b8ee391cc8ebde103c6f43c44633c64c63dc
Ah Heng <leesekheng@yahoo.com>
2017-02-14 19:43:02 +0800

    Successful deletion and saving to database.

--- Assets/Scripts/Splashpage Scripts/SplashScripts.cs
+++ Assets/Scripts/Splashpage Scripts/SplashScripts.cs
@@ -13,50 +13,15 @@ public class SplashScripts : MonoBehaviour {
     public float m_timer = 3.0f;
     private float timeCounter = 0;
 
-    private string connectionString = "URI=file:" + Application.dataPath + "/DataBase/AllData.db";   //Path to database.
+    private string connectionString;   //Path to database.
 
-	// Use this for initialization
+    // Use this for initialization
 	void Start () {
-        //IDbConnection dbconn;
-        //dbconn = (IDbConnection)new SqliteConnection(connectionString);
-        //dbconn.Open(); //Open connection to the database.
-        //IDbCommand dbcmd = dbconn.CreateCommand();
-        //string sqlQuery = "INSERT INTO PlaceSequence(Value,PlayerDamage,Name) VALUES(20,100,'YOLO');";
-        //dbcmd.CommandText = sqlQuery;
-        //IDataReader reader = dbcmd.ExecuteReader();
-        //Debug.Log("Saving to SQL Table");
-        //reader.Close();
-        //reader = null;
-        //dbcmd.Dispose();
-        //dbcmd = null;
-        //dbconn.Close();
-        //dbconn = null;
-
-        // Reading from DataBase
-        //IDbConnection dbconn;
-        //dbconn = (IDbConnection)new SqliteConnection(connectionString);
-        //dbconn.Open(); //Open connection to the database.
-        ////IDbCommand dbcmd = dbconn.CreateCommand();
-        //dbcmd = dbconn.CreateCommand();
-        ////string sqlQuery = "SELECT * " + "FROM PlaceSequence";
-        //string sqlQuery2 = "SELECT * " + "FROM PlaceSequence";
-        //dbcmd.CommandText = sqlQuery2;
-        //reader = dbcmd.ExecuteReader();
-        //Debug.Log("Reading SQL Table");
-        //while (reader.Read())
-        //{
-        //    int value = reader.GetInt32(0);
-        //     int rand = reader.GetInt32(1);
-        //   string name = reader.GetString(2);
-        //    //string allTheStuff = reader.GetString(3)
-        //    Debug.Log("value= " + value + ", Score=" +rand + ", name=" + name);
-        //}
-        //reader.Close();
-        //reader = null;
-        //dbcmd.Dispose();
-        //dbcmd = null;
-        //dbconn.Close();
-        //dbconn = null;
+        connectionString = "URI=file:" + Application.dataPath + "/DataBase/AllData.db";
+        //Debug.Log(Application.dataPath);
+        //SaveToDataBase();
+        //TryDeleteToDataBase();
+        ReadFromDataBase();
 	}
 	
 	// Update is called once per frame
@@ -65,4 +30,69 @@ public class SplashScripts : MonoBehaviour {
         if (timeCounter > m_timer)
             gameObject.GetComponent<GoSceneScript>().GoNextScene(); // Will change soon!
 	}
+    void ReadFromDataBase()
+    {
+        // Reading from DataBase
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery2 = "SELECT * FROM PlaceSequence";
+        dbcmd.CommandText = sqlQuery2;
+        IDataReader reader = dbcmd.ExecuteReader();
+        Debug.Log("Reading SQL Table");
+        while (reader.Read())
+        {
+            // The Order is Important!
+            int value = reader.GetInt32(0);
+            int rand = reader.GetInt32(1);
+            string name = reader.GetString(2);
+            Debug.Log("value= " + value + ", Score=" + rand + ", name=" + name);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    void SaveToDataBase()
+    {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        using (IDbCommand dbcmd = dbconn.CreateCommand())
+        {
+            //Debug.Log("Saving to SQL Table");
+            // This is basically inserting the Coloumn where there is PlayerDamage and Name.
+            string sqlQuery = String.Format("INSERT INTO PlaceSequence(PlayerDamage,Name) VALUES(\"{0}\",\"{1}\")", 100, String.Format("YOLO"));
+
+            //string sqlQuery = String.Format("INSERT INTO PlaceSequence(Value,PlayerDamage) VALUES(\"{0}\",\"{1}\")", 20, 100);
+            dbcmd.CommandText = sqlQuery;
+            //Debug.Log("Trying to Save");
+            dbcmd.ExecuteScalar();
+            //Debug.Log("Successful Saving");
+            dbcmd.Dispose();
+            //dbcmd = null;
+        }
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    void TryDeleteToDataBase()
+    {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        using (IDbCommand dbcmd = dbconn.CreateCommand())
+        {
+            string sqlQuery = String.Format("DELETE FROM PlaceSequence WHERE Value = \"{0}\"", 2);
+            dbcmd.CommandText = sqlQuery;
+            dbcmd.ExecuteScalar();
+            dbcmd.Dispose();
+        }
+        dbconn.Close();
+        dbconn = null;
+    }
 }
