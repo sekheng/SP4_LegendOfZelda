c28f10667f6a1a00718a58024ecf6cfdf48ee00b
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-02-15 11:07:22 +0800

    grid for A*

--- Assets/Scripts/AI/Grid.cs
+++ Assets/Scripts/AI/Grid.cs
@@ -7,4 +7,51 @@ public class Grid : MonoBehaviour{
     public Vector2 gridWorldSize;
     public float nodeRadius;
     Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector2 worldBottomLeft = (Vector2)transform.position - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+
+        for(int x = 0; x < gridSizeX; ++x)
+        {
+            for(int y = 0; y < gridSizeY; ++y)
+            {
+                Vector2 worldPoint = worldBottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+                //collision check
+                bool walkable = (Physics2D.OverlapCircle(worldPoint, nodeRadius, unwalkableMask) == null);
+
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(gridWorldSize.x, gridWorldSize.y));
+        if(grid != null)
+        {
+            foreach(Node node in grid)
+            {
+                Gizmos.color = Color.red;
+                if(node.m_bWalkable)//change color if walkable
+                {
+                    Gizmos.color = Color.white;
+                }
+                Gizmos.DrawCube(node.m_v2_worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
 }
