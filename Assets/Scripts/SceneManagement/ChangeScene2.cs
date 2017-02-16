66fe8cd65231b4e546f5b8e3bf6997722b6ab259
QINGPING <qingping1998@gmail.com>
2017-02-16 14:10:13 +0800

    CHANGELOG:
    Implemented all scenes and allowed for teleportation between all scenes.
    
    TODO:
    
    MainMenu and options and stuff
    SceneTransitions;
    
    ISSUES:
    
    Need to save the dragon's conversationcount into SQLite

---
+++ Assets/Scripts/SceneManagement/ChangeScene2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene2 : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) //mostly is just for compass
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
