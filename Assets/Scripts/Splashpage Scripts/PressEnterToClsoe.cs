8ad676f6d67c7a1fede95bbc0f7d9a5c82f3097c
QINGPING <qingping1998@gmail.com>
2017-02-17 16:54:39 +0800

    no message

--- Assets/Scripts/Splashpage Scripts/PressEnterToClsoe.cs
+++ Assets/Scripts/Splashpage Scripts/PressEnterToClsoe.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PressEnterToClsoe : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.Return) && gameObject.activeSelf)
+	    if(Input.GetKeyDown(KeyCode.Return) && gameObject.activeSelf && SceneManager.GetActiveScene().buildIndex == 0)
         {
             gameObject.SetActive(false);
         }
