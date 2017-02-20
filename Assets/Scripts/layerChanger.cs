ff85611fc28daa95d89bc8bd0212c19c6617f35b
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-02-20 14:47:56 +0800

    changed CSVreader, added golemboss anims layer changer script and fsm for golemboss

---
+++ Assets/Scripts/layerChanger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class layerChanger : MonoBehaviour {
+    private GameObject player;
+    private SpriteRenderer spriteR;
+    private SpriteRenderer playerSpriteR;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerSpriteR = player.transform.FindChild("Actual Sprite").GetComponent<SpriteRenderer>();
+        }
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                playerSpriteR = player.transform.FindChild("Actual Sprite").GetComponent<SpriteRenderer>();
+            }
+        }
+        else
+        {
+            if(player.transform.position.y > gameObject.transform.position.y)
+            {
+                spriteR.sortingOrder = playerSpriteR.sortingOrder + 1;
+            }
+            else
+            {
+                spriteR.sortingOrder = playerSpriteR.sortingOrder - 1;
+            }
+        }
+    }
+}
