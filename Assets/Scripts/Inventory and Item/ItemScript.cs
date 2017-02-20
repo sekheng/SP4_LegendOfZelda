0c8dfa58d91997779bb8791effd0ea80d52c6868
Ah Heng <leesekheng@yahoo.com>
2017-02-20 23:21:34 +0800

    In the progress of saving data.

---
+++ Assets/Scripts/Inventory and Item/ItemScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour {
+    // For the item information
+    public itemInformation m_itemInform = new itemInformation();
+
+    [Tooltip("Item's name")]
+    public string m_itemName;
+    [Tooltip("Item's type")]
+    public string m_itemType;
+    [Tooltip("Item's effect")]
+    public string m_itemEffect;
+    [Tooltip("How many items")]
+    public int m_numOfItems = 1;
+
+	// Use this for initialization
+	void Start () {
+        m_itemInform.item_name = m_itemName;
+        m_itemInform.item_type = m_itemType;
+        m_itemInform.item_effect = m_itemEffect;
+        m_itemInform.item_count = m_numOfItems;
+	}
+}
