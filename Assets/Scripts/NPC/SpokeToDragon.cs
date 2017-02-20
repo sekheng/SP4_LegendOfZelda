e85b1dc7f46fe6d145d43945d07ddd9d0292f881
QINGPING <qingping1998@gmail.com>
2017-02-20 15:35:15 +0800

    CHANGELOG:
    Added DragonAnim
    Tweaked the Main Camera to showoff our cool art (Thanks Kelly)
    Tweaked VIDE to properly update whether we are done talking.
    Remove Deprecated Scripts.

---
+++ Assets/Scripts/NPC/SpokeToDragon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpokeToDragon : MonoBehaviour {
+
+    private Animator myAnim;
+
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(LocalDataSingleton.instance.talkedToDragon)
+        {
+            myAnim.Play("DragonSleeping");
+        }
+	}
+}
