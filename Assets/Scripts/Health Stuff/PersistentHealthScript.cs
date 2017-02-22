af9bc86cbea51ad89c9c0e6d5ae704a47390844d
Ah Heng <leesekheng@yahoo.com>
2017-02-22 19:55:08 +0800

    Fixed the inventory UI for player, and a shitty fix for player's health getting from the persistent health data.

--- Assets/Scripts/Health Stuff/PersistentHealthScript.cs
+++ Assets/Scripts/Health Stuff/PersistentHealthScript.cs
@@ -6,23 +6,36 @@ using System.Collections;
 /// Make sure the player's health will not restart!
 /// </summary>
 public class PersistentHealthScript : MonoBehaviour {
-    public static PersistentHealthScript instance = null;
+    public static PersistentHealthScript instance
+    {
+        get
+        {
+            if (CantTouchThis == null)
+            {
+                CantTouchThis = FindObjectOfType<PersistentHealthScript>();
+                CantTouchThis.Start();
+            }
+            return CantTouchThis;
+        }
+    }
+    private static PersistentHealthScript CantTouchThis;
 
     // Make sure the player's health won't change!
     private float max_health = 100;
-    public float currHealth
-    {
-        get;
-        set;
-    }
+    public float currHealth = 100;
     private static string playerTableName = "PlayerStuff";
+    private bool hasInitialized = false;
 
 	// Use this for initialization
 	void Start () {
-        instance = this;
-        string[] zeCondition = { "PlayerID = " + HeroDataScript.m_playerID };
-        max_health = MySQLiteHandler.instance.getFloat(playerTableName, "PlayerHealth", zeCondition);
-        currHealth = max_health;
+        Debug.Log("Starting the persistent health");
+        if (!hasInitialized)
+        {
+            string[] zeCondition = { "PlayerID = " + HeroDataScript.m_playerID };
+            max_health = MySQLiteHandler.instance.getFloat(playerTableName, "PlayerHealth", zeCondition);
+            currHealth = max_health;
+            hasInitialized = true;
+        }
     }
 
     public float getMaxHealth()
--- Assets/Scripts/Heroes Stuff/HeroDataScript.cs
+++ Assets/Scripts/Heroes Stuff/HeroDataScript.cs
@@ -11,19 +11,47 @@ public class HeroDataScript : MonoBehaviour {
     private static string playerTableName = "PlayerStuff";
     // Hero's health component
     private HealthScript herosHealth;
+    // Unfortunately, everything has become so hardcoded to the point that it has to come to this!
+    private bool m_GotHealth = false;
 
 	// Use this for initialization
 	void Start () {
         herosHealth = GetComponent<HealthScript>();
-        herosHealth.resetToMaxHealth(PersistentHealthScript.instance.getMaxHealth());
-        herosHealth.m_health = PersistentHealthScript.instance.currHealth;
+        //Debug.Log("Get the persistent health");
+        //herosHealth.resetToMaxHealth(PersistentHealthScript.instance.getMaxHealth());
+        //herosHealth.m_health = PersistentHealthScript.instance.currHealth;
+        //Debug.Log("Succeed in getting the persistent health");
         //string[] zeCondition = {"PlayerID = " + m_playerID};
         //herosHealth.m_health = MySQLiteHandler.instance.getFloat(playerTableName, "PlayerHealth", zeCondition);
+        getHealthFromPersistent();
 	}
+
+    // So that it won't be so intensive on the Update loop!
+    void FixedUpdate()
+    {
+        getHealthFromPersistent();
+    }
 
     // When this script is about to destroy, send it to the singleton!
     void OnDisable()
     {
         PersistentHealthScript.instance.currHealth = herosHealth.m_health;
+    }
+
+    void getHealthFromPersistent()
+    {
+        switch (m_GotHealth)
+        {
+            case false:
+                if (PersistentHealthScript.instance != null)
+                {
+                    m_GotHealth = true;
+                    herosHealth.resetToMaxHealth(PersistentHealthScript.instance.getMaxHealth());
+                    herosHealth.m_health = PersistentHealthScript.instance.currHealth;
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
