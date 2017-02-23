d67b814a4af14978bd8f23144ed10cece1d79c6f
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-02-24 01:04:49 +0800

    trying to implement audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour {
+
+    
+    public Slider slider;
+    private AudioSource[] arrayOfAudioSource;
+    //private 
+    // Use this for initialization
+    void Start () {
+        arrayOfAudioSource = GetComponentsInChildren<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
