d67a64c140e0e1200d7843997f3daa200e1afe0b
unknown <152063Q@M313G1102.sidm.student.local>
2017-02-23 13:00:43 +0800

    CHANGELOG:
    Work on extra scenes after lunch thanks.

--- Assets/Scripts/Splashpage Scripts/PressEnterToClsoe.cs
+++ Assets/Scripts/Splashpage Scripts/PressEnterToClsoe.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class PressEnterToClsoe : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.Return) && gameObject.activeSelf && SceneManager.GetActiveScene().buildIndex == 0)
+        if (Input.GetKeyDown(KeyCode.Return) && gameObject.activeSelf && (SceneManager.GetActiveScene().buildIndex == 0 || SceneManager.GetActiveScene().buildIndex == 1))
         {
             gameObject.SetActive(false);
         }
