1741cc6bfb4efd17ff694885a443baeead4d2d3b
unknown <152063Q@M313G1102.sidm.student.local>
2017-02-28 12:36:46 +0800

    CHANGELOG: LDS MODIFIED: ALERT PLEASE TEST ANDROID.
    Importing new assets
    removing old assets

--- Assets/Scripts/AI/Slime/Slime_DeadState.cs
+++ Assets/Scripts/AI/Slime/Slime_DeadState.cs
@@ -6,6 +6,8 @@ public class Slime_DeadState : State {
     //public SpriteRenderer sprite;
     private float accumTimeToSetInactive;
 
+    public GameObject[] relicArray; // please drag in the prefabs.
+
     void Start()
     {
         accumTimeToSetInactive = 0.0f;
@@ -21,6 +23,11 @@ public class Slime_DeadState : State {
         monsterSpriteRenderer.color = new Color(monsterSpriteRenderer.color.r, monsterSpriteRenderer.color.g, monsterSpriteRenderer.color.b, (timeToSetInactive - accumTimeToSetInactive));
         if(accumTimeToSetInactive > timeToSetInactive)
         {
+            if (relicArray.Length > 0 && (Random.Range(0, 10) == 0)) //we insert a 10% chance for a relic to spawn.
+            {
+                int randomIndex = Random.Range(0, relicArray.Length);
+                Instantiate(relicArray[randomIndex], monsterTransform.position, Quaternion.identity);
+            }
             Destroy(monsterTransform.gameObject);
         }
     }
