ec58b329327226cb4a07686b906cd0d59f1b52cd
unknown <152063Q@M313G1102.sidm.student.local>
2017-02-28 13:42:21 +0800

    CHANGELOG:
    Standardised play menu.

--- Assets/Scripts/Splashpage Scripts/PlayMenuControl.cs
+++ Assets/Scripts/Splashpage Scripts/PlayMenuControl.cs
@@ -3,18 +3,10 @@ using UnityEngine.UI;
 
 public class PlayMenuControl : MonoBehaviour {
 
-    public Transform NewGame, Load1, Load2, Load3;
+    public GameObject[] stuff;
+    private int lookAt;
     private SoundEffectsManager soundEffects;
 
-    public enum CURRENTBUTTON
-    {
-        NEWGAME,
-        LOAD1,
-        LOAD2,
-        LOAD3,
-    };
-
-    public CURRENTBUTTON selected;
 #if UNITY_STANDALONE
 	// Use this for initialization
 	void Start () {
@@ -23,11 +15,8 @@ public class PlayMenuControl : MonoBehaviour {
             soundEffects = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<SoundEffectsManager>();
 
         }
-        selected = PlayMenuControl.CURRENTBUTTON.NEWGAME;
-        NewGame.GetComponent<Button>().interactable = false;
-        Load1.GetComponent<Button>().interactable = false;
-        Load2.GetComponent<Button>().interactable = false;
-        Load3.GetComponent<Button>().interactable = false;
+        lookAt = 0;
+        GetComponent<RectTransform>().anchoredPosition = stuff[lookAt].GetComponent<RectTransform>().anchoredPosition;
 	}
 	
 	// Update is called once per frame
@@ -37,178 +26,43 @@ public class PlayMenuControl : MonoBehaviour {
             soundEffects = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<SoundEffectsManager>();
 
         }
-        if (!LocalDataSingleton.instance.transform.GetChild(0).gameObject.activeSelf)
+
+        // navigation
+        if (Input.GetKeyDown(KeyCode.UpArrow) && lookAt > 0)
         {
-            if (selected == CURRENTBUTTON.NEWGAME)
+            if (soundEffects != null)
             {
-                NewGame.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                NewGame.GetComponent<Button>().interactable = true;
+                soundEffects.playPositiveSound();
 
-                Load1.localScale = new Vector3(1, 1, 1);
-                Load1.GetComponent<Button>().interactable = false;
-
-                Load2.localScale = new Vector3(1, 1, 1);
-                Load2.GetComponent<Button>().interactable = false;
-
-                Load3.localScale = new Vector3(1, 1, 1);
-                Load3.GetComponent<Button>().interactable = false;
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    selected = CURRENTBUTTON.LOAD3;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    selected = CURRENTBUTTON.LOAD1;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    //LocalDataSingleton.instance.GoNext();
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                    NewGame.BroadcastMessage("beginLoadingData");
-                }
             }
-            else if (selected == CURRENTBUTTON.LOAD1)
+            --lookAt;
+            GetComponent<RectTransform>().anchoredPosition = stuff[lookAt].GetComponent<RectTransform>().anchoredPosition;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && lookAt < (stuff.Length - 1))
+        {
+            if (soundEffects != null)
             {
-                NewGame.localScale = new Vector3(1, 1, 1);
-                NewGame.GetComponent<Button>().interactable = false;
+                soundEffects.playPositiveSound();
 
-                Load1.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                Load1.GetComponent<Button>().interactable = true;
-
-                Load2.localScale = new Vector3(1, 1, 1);
-                Load2.GetComponent<Button>().interactable = false;
-
-                Load3.localScale = new Vector3(1, 1, 1);
-                Load3.GetComponent<Button>().interactable = false;
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    selected = CURRENTBUTTON.NEWGAME;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    selected = CURRENTBUTTON.LOAD2;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    //ADD CODES FOR THE LOADING.
-                    Load1.BroadcastMessage("beginLoadingData");
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
             }
-            else if (selected == CURRENTBUTTON.LOAD2)
+            ++lookAt;
+            GetComponent<RectTransform>().anchoredPosition = stuff[lookAt].GetComponent<RectTransform>().anchoredPosition;
+        }
+
+        //press enter
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (soundEffects != null)
             {
-                NewGame.localScale = new Vector3(1, 1, 1);
-                NewGame.GetComponent<Button>().interactable = false;
+                soundEffects.playPositiveSound();
 
-                Load1.localScale = new Vector3(1, 1, 1);
-                Load1.GetComponent<Button>().interactable = false;
-
-                Load2.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                Load2.GetComponent<Button>().interactable = true;
-
-                Load3.localScale = new Vector3(1, 1, 1);
-                Load3.GetComponent<Button>().interactable = false;
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    selected = CURRENTBUTTON.LOAD1;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    selected = CURRENTBUTTON.LOAD3;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    //ADD CODES FOR THE LOADING.
-                    Load2.BroadcastMessage("beginLoadingData");
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
             }
-            else if (selected == CURRENTBUTTON.LOAD3)
+            for (int i = 0; i < stuff.Length; i++ )
             {
-                NewGame.localScale = new Vector3(1, 1, 1);
-                NewGame.GetComponent<Button>().interactable = false;
-
-                Load1.localScale = new Vector3(1, 1, 1);
-                Load1.GetComponent<Button>().interactable = false;
-
-                Load2.localScale = new Vector3(1, 1, 1);
-                Load2.GetComponent<Button>().interactable = false;
-
-                Load3.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                Load3.GetComponent<Button>().interactable = true;
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
+                if(i == lookAt)
                 {
-                    selected = CURRENTBUTTON.LOAD2;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    selected = CURRENTBUTTON.NEWGAME;
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    //ADD CODES FOR THE LOADING.
-                    Load3.BroadcastMessage("beginLoadingData");
-                    if (soundEffects != null)
-                    {
-                        soundEffects.playPositiveSound();
-
-                    }
+                    stuff[i].GetComponent<LoadGameScript>().BroadcastMessage("beginLoadingData");
+                    break;
                 }
             }
         }
