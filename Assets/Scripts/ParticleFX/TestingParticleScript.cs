68c28f8b23d1666dba39c3ed1c4dc2cee4dc689b
Ah Heng <leesekheng@yahoo.com>
2017-03-01 15:36:38 +0800

    Particle effects for the rock should be fixed.

--- Assets/Scripts/ParticleFX/TestingParticleScript.cs
+++ Assets/Scripts/ParticleFX/TestingParticleScript.cs
@@ -11,10 +11,17 @@ public class TestingParticleScript : MonoBehaviour {
     public float m_timeInterval = 0.5f;
     // We need a time counter
     private float m_timeCounter = 0;
+    [Tooltip("Play how many times at a time")]
+    public int m_times = 2;
+    [Tooltip("How many spacing should the number of times be player in X-axis")]
+    public float m_SpacingX = 50.0f;
+    // we need to know it's original position
+    private Vector3 particleSystemPos;
 
 	// Use this for initialization
 	void Start () {
         m_TheParticleSystem = GetComponent<ParticleScript>();
+        particleSystemPos = transform.position;
 	}
 	
     void Update()
@@ -22,7 +29,13 @@ public class TestingParticleScript : MonoBehaviour {
         m_timeCounter += Time.deltaTime;
         if (m_timeCounter > m_timeInterval)
         {
-            m_TheParticleSystem.playEffect();
+            for (int zeNum = 0; zeNum < m_times; ++zeNum)
+            {
+                m_TheParticleSystem.stopEffect();
+                m_TheParticleSystem.playEffect();
+                transform.position = new Vector3(transform.position.x + m_SpacingX, transform.position.y, transform.position.z);
+            }
+            transform.position = particleSystemPos;
             m_timeCounter = 0;
         }
     }
