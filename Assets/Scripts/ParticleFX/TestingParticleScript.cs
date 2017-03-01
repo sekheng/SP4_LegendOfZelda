532a9df552c66569bf6df088a992b9bb86c7bafd
Ah Heng <leesekheng@yahoo.com>
2017-03-01 13:00:35 +0800

    Struggling to solve the particle effects bug.

--- Assets/Scripts/ParticleFX/TestingParticleScript.cs
+++ Assets/Scripts/ParticleFX/TestingParticleScript.cs
@@ -5,10 +5,24 @@ using System.Collections;
 /// This is only used for 
 /// </summary>
 public class TestingParticleScript : MonoBehaviour {
+    // We need to test out whether is there any thing!
+    private ParticleScript m_TheParticleSystem;
+    [Tooltip("What is the time interval to play the particles")]
+    public float m_timeInterval = 0.5f;
+    // We need a time counter
+    private float m_timeCounter = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+        m_TheParticleSystem = GetComponent<ParticleScript>();
 	}
 	
+    void Update()
+    {
+        m_timeCounter += Time.deltaTime;
+        if (m_timeCounter > m_timeInterval)
+        {
+            m_TheParticleSystem.playEffect();
+        }
+    }
 }
